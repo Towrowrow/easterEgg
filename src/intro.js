35f14d77379686c3a9eae3153a08edc64b82ea5c
Tilmant Rémi <towtowrow@gmail.com>
2019-04-18 14:56:24 +0200

    intro ok

--- src/intro.js
+++ src/intro.js
@@ -1,20 +1,42 @@
 import React, { Component } from 'react';
 
-class App extends Component {
-  constructor(props) {
-    super(props)
-
-  }
 
 
+
+class Intro extends Component {
 
 
   render() {
     return (
-      <div className="App">
+      <div>
+        <div className="ButtonSW">
+          <button className="BoutonSW">JOUER</button>
+        </div>
+        <section className="star-wars">
 
-        );
-      }
-    }
-    }
-    export default App;
+          <div className="crawl">
+            <div className="title">
+              <p>Episode I</p>
+              <h1>La jeunesse de Tete d'Oeuf</h1>
+            </div>
+
+            <p>Dans une galaxie lointaine, trés lointaine, une équipe d'Oeufs de l'espace commence son voyage.
+  Aprés moult aventures spatiales, les Oeufs arrivent sur Terre, et c'est pas pour tailler le bout de gras.
+          </p>
+
+            <p>Prennez place dans un Jeu révolutionnaire et combattez pour sauver votre civilisation.
+              Tuez quicquonce se mettras en travers de votre chemin.
+          </p>
+
+            <p>Vous êtes notre dernier espoir, Capitaine Tête d'oeuf, menez vos troupes à la victoire !….</p>
+          </div>
+        </section>
+
+      </div >
+    );
+  }
+}
+
+
+
+export default Intro;
