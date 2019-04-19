a83cb5dded98cf9ff6535bc2245839fc9002a8f8
Lea Godard <lea.godard.web@gmail.com>
2019-04-19 09:22:04 +0200

    test shake

--- src/attack.js
+++ src/attack.js
@@ -1,41 +1,46 @@
-import React from 'react';
+import React, { Component } from 'react';
 
 
+class Attack extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {}
 
-
-const Attack = (props) => {
-
-
-  const cliked = () => {
-    return (
-      props.victory(props.eggs, props.monsters)
-    )
 
   }
 
-  return (
-    <div className="card mb-3" >
-      <div className="row no-gutters">
-        <div className="col-md-4 align-self-center">
-          <img
-            src="https://image.flaticon.com/icons/svg/1541/1541691.svg"
-            className="card-img my-4 shake" alt="swords"
-          />
-        </div>
-        <div className="col-md-8">
-          <div className="card-body ">
-            <p>
-              <button
-                onClick={() => cliked()}
-                type="button"
-                className="btn btn-dark btn-block mt-5">Attack !
+  cliked = () => {
+    return (
+      this.props.victory(this.props.eggs, this.props.monsters)
+    )
+  }
+
+  render() {
+    return (
+      <div className="card mb-3" >
+        <div className="row no-gutters">
+          <div
+            className="col-md-4 align-self-center">
+            <img
+              src="https://image.flaticon.com/icons/svg/1541/1541691.svg"
+              className="card-img my-4 shake" alt="swords"
+            />
+          </div>
+          <div className="col-md-8">
+            <div className="card-body ">
+              <p>
+                <button
+                  onClick={() => this.cliked()}
+                  type="button"
+                  className="btn btn-dark btn-block mt-5">Attack !
               </button>
-            </p>
+              </p>
+            </div>
           </div>
         </div>
-      </div>
-    </div >
-  )
+      </div >
+    )
+  }
 };
 
 export default Attack;
