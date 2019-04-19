7f757c574b6838df7a125e3ddc1b24693ff6fa68
Lea Godard <lea.godard.web@gmail.com>
2019-04-19 10:11:10 +0200

    pas cassé

--- src/attack.js
+++ src/attack.js
@@ -1,30 +1,44 @@
 import React, { Component } from 'react';
+import {
+  Animated,
+  BounceAnimations
+} from "animated-styled-components";
 
 
 class Attack extends Component {
   constructor(props) {
     super(props)
-    this.state = {}
-
-
+    this.state = {
+      animate: false
+    }
+    this.cliked = this.cliked.bind(this);
   }
 
-  cliked = () => {
-    return (
-      this.props.victory(this.props.eggs, this.props.monsters)
-    )
+  cliked() {
+    this.props.victory(this.props.eggs, this.props.monsters);
+    this.setState({
+      animate: true,
+    })
   }
 
   render() {
     return (
       <div className="card mb-3" >
         <div className="row no-gutters">
-          <div
-            className="col-md-4 align-self-center">
-            <img
-              src="https://image.flaticon.com/icons/svg/1541/1541691.svg"
-              className="card-img my-4 shake" alt="swords"
-            />
+
+          <div className="col-md-4 align-self-center">
+            <Animated
+              animation={{
+                in: BounceAnimations.BounceInForwards,
+                duration_in: 1
+              }}
+            >
+              <img
+                src="https://image.flaticon.com/icons/svg/1541/1541691.svg"
+                className="card-img my-4 shake" alt="swords"
+              />
+
+            </Animated>
           </div>
           <div className="col-md-8">
             <div className="card-body ">
