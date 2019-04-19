7df35f9fbc72f45249cf69553f56b8284ca50887
Lea Godard <lea.godard.web@gmail.com>
2019-04-19 11:14:16 +0200

    shake

--- src/attack.js
+++ src/attack.js
@@ -5,19 +5,29 @@ import {
 } from "animated-styled-components";
 
 
+
 class Attack extends Component {
   constructor(props) {
     super(props)
     this.state = {
-      animate: false
+      animate: true
     }
     this.cliked = this.cliked.bind(this);
+  }
+
+  componentDidUpdate(prevProps, prevState) {
+    if (prevState.animate !== this.state.animate) {
+      this.setState({
+        animate: true
+      })
+    }
+
   }
 
   cliked() {
     this.props.victory(this.props.eggs, this.props.monsters);
     this.setState({
-      animate: true,
+      animate: false,
     })
   }
 
@@ -27,18 +37,24 @@ class Attack extends Component {
         <div className="row no-gutters">
 
           <div className="col-md-4 align-self-center">
-            <Animated
-              animation={{
-                in: BounceAnimations.BounceInForwards,
-                duration_in: 1
-              }}
-            >
+            {this.state.animate ?
+              <Animated
+                animation={{
+                  in: BounceAnimations.BounceInForwards,
+                  duration_in: 1
+                }}
+              >
+                <img
+                  src="https://image.flaticon.com/icons/svg/1541/1541691.svg"
+                  className="card-img my-4 shake" alt="swords"
+                />
+
+              </Animated> :
               <img
                 src="https://image.flaticon.com/icons/svg/1541/1541691.svg"
                 className="card-img my-4 shake" alt="swords"
               />
-
-            </Animated>
+            }
           </div>
           <div className="col-md-8">
             <div className="card-body ">
