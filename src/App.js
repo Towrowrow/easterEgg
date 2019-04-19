0ee6b7f3850f9a54ce49a9a47ac0b63e7f259e89
Lea Godard <lea.godard.web@gmail.com>
2019-04-19 13:41:08 +0200

    cassé le sudo

--- src/App.js
+++ src/App.js
@@ -179,7 +179,6 @@ class App extends Component {
   // api calls
   apiEggs() {
     if (this.state.userName === 'sudo') {
-      this.fixCSS();
       this.setState({
         nameEgg: "Le GRAND GOUROUUU!",
         imageEgg: gourou,
